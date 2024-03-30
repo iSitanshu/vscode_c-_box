59bcc8f894e11b289eb46012f39a71ed9c0773e6
iSitanshu <111778981+iSitanshu@users.noreply.github.com>
2024-03-30 07:37:34 +0000

    Commit day 2

--- index.cpp
+++ index.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main(){
     cout<<"Sitanshu mishra"<<endl;
     int n;
     cin>>n;
     cout<<n<<endl;
+    int sum=pow(n,2);
+    cout<<sum<<endl;
+    return 0;
 }
