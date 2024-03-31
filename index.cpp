33d99ca07eb2e7e8ebb8c894f8675949201b8739
iSitanshu <111778981+iSitanshu@users.noreply.github.com>
2024-03-31 03:29:31 +0000

    Commit 3

--- index.cpp
+++ index.cpp
@@ -3,10 +3,9 @@
 using namespace std;
 int main(){
     cout<<"Sitanshu mishra"<<endl;
-    int n;
-    cin>>n;
-    cout<<n<<endl;
+    int n=10;
     int sum=pow(n,2);
+    cout<<"day 3"<<endl;
     cout<<sum<<endl;
     return 0;
 }
